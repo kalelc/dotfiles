0749945fce571792f4e550df9926761a9d8d1107
Andres Colonia <andreskal@gmail.com>
2018-10-12 15:24:45 +0000

    include install script

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Get dotfiles installation directory
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+ln -sf "$DOTFILES_DIR/gitconfig" ~/.gitconfig
+ln -sf "$DOTFILES_DIR/gitignore_global" ~/.gitignore_global
+ln -sf "$DOTFILES_DIR/zshrc" ~/.zshrc
+ln -sf "$DOTFILES_DIR/exports" ~/.exports
+ln -sf "$DOTFILES_DIR/aliases" ~/.aliases
+ln -sf "$DOTFILES_DIR/vimrc" ~/.vimrc
